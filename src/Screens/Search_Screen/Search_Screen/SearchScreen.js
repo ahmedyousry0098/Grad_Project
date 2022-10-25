e1cec0110a81c2dd23dda8f6d20f223ea56e7aa1
Ahmed Yousry <97663058+ahmedyousry0098@users.noreply.github.com>
2022-10-25 10:00:36 +0200

    Add files via upload

---
+++ src/Screens/Search_Screen/Search_Screen/SearchScreen.js
@@ -0,0 +1,74 @@
+import { useState, useEffect, useContext } from "react";
+import BookShelfChanger from "../../Components/BookShelfChanger";
+import Context from "../../Context/context";
+
+const SearchScreen = ({onSwitch}) => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const {state: {search_books}, getSearchedResult} = useContext(Context)
+
+  useEffect(() => {
+    
+    if (searchTerm) {
+      getSearchedResult(searchTerm)
+    }
+  }, [searchTerm])  
+
+  return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <div 
+            href
+            className="close-search"
+            onClick={onSwitch}
+          >
+            Close
+          </div>
+          
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title, author, or ISBN"
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {
+              search_books? search_books.map((book) => {
+                return <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div 
+                          className="book-cover"
+                          style={{
+                            width: 150,
+                            height: 220,
+                            backgroundImage: `url(${book.imageLinks.thumbnail})`
+                          }}
+                        ></div>
+
+                        <BookShelfChanger 
+                          selected={book.id}
+                        />
+                      </div>
+
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.authors}</div>
+
+                    </div>
+                  </li>
+              }): null
+            } 
+          </ol>
+          
+        </div>
+      </div>
+  )
+};
+
+export default SearchScreen;
